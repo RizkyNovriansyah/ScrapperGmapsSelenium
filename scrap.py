5688fda6a0be5f168cb7e403ff5fbb9debb7bcf9
Rizky Novriansyah <rizkynovriansyah82.rn@gmail.com>
2020-10-29 11:11:40 +0700

    detail item

--- scrap.py
+++ scrap.py
@@ -14,17 +14,46 @@ key = "Toko Sepatu"
 
 final_msg = key+", "+kota
 ActionChains(driver).send_keys(final_msg).perform()
-
 class_find = 'searchbox-searchbutton'
 driver.find_element_by_class_name(class_find).click()
 
 # 
-time.sleep(4)
+# time.sleep(4)
 class_item = 'section-result'
-items = driver.find_elements_by_class_name(class_item)
-time.sleep(2)
+items = []
+while len(items) == 0 :
+    items = driver.find_elements_by_class_name(class_item)
+
 print(len(items))
+count = 0
 # print(items)
+for item in items:
+    count += 1
+    print("== Detail Item ==")
+    item.click()
+    print("Title : ")
+    
+    # time.sleep(4)
+    val = []
+    while len(val) == 0 :
+        val = driver.find_elements_by_class_name('section-hero-header-title-title')
+    title = val[0].find_element_by_tag_name('span')
+    print(title.text)
+
+    print("Back To list")
+    class_back = 'section-back-to-list-button'
+    driver.find_element_by_class_name(class_back).click()
+    time.sleep(4)
+
+    class_item = 'section-result'
+    items = driver.find_elements_by_class_name(class_item)
+    # time.sleep(4)
+
+    for x in range(count):
+        items.pop(0)
+    
+    print(len(items))
+
 
 
 # ActionChains(driver).key_up(Keys.ENTER).perform()
