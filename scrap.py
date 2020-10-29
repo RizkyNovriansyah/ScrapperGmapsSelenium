72e0cfa7a967a32bff3dc0f080fb2368558a403d
Rizky Novriansyah <rizkynovriansyah82.rn@gmail.com>
2020-10-29 14:19:44 +0700

    fixing detail

--- scrap.py
+++ scrap.py
@@ -3,12 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+from openpyxl import Workbook
+import os
+# from time import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.google.com/maps')
 
 # kota = input('kota : ')
 # key = input('key : ')
+time.sleep(2)
 kota = "Jakarta"
 key = "Toko Sepatu"
 
@@ -17,23 +21,27 @@ ActionChains(driver).send_keys(find_key).perform()
 class_find = 'searchbox-searchbutton'
 driver.find_element_by_class_name(class_find).click()
 
-#
-# time.sleep(4)
-class_item = 'section-result'
-items = []
-while len(items) == 0:
-    items = driver.find_elements_by_class_name(class_item)
-
-print(len(items))
-count = 0
 nama_tokos = []
-for item in items:
-    temp_title = []
-    while len(temp_title) == 0:
-        temp_title = item.find_elements_by_class_name('section-result-title')
-    title = temp_title[0].find_element_by_tag_name('span')
-    nama_toko = title.text
-    nama_tokos.append(nama_toko)
+for x in range(2):
+    time.sleep(4)
+    class_item = 'section-result'
+    items = []
+    while len(items) == 0:
+        items = driver.find_elements_by_class_name(class_item)
+
+    # print(len(items))
+    for item in items:
+        temp_title = []
+        while len(temp_title) == 0:
+            temp_title = item.find_elements_by_class_name('section-result-title')
+        title = temp_title[0].find_element_by_tag_name('span')
+        nama_toko = title.text
+        nama_tokos.append(nama_toko)
+
+    driver.find_element_by_class_name('n7lv7yjyC35__button-next-icon').click()
+    
+print("Data Recorded : ")
+print(len(nama_tokos))
 
 class_close = 'gsst_a'
 driver.find_element_by_class_name(class_close).click()
@@ -44,8 +52,6 @@ for nama_toko in nama_tokos:
     if final_msg == find_key:
         continue
     
-    print("Find : ")
-    print(final_msg)
     ActionChains(driver).send_keys(final_msg).perform()
     class_find = 'searchbox-searchbutton'
     driver.find_element_by_class_name(class_find).click()
@@ -75,37 +81,61 @@ for nama_toko in nama_tokos:
                 print(x)
         
     detail = {}
+    try:
+        title = temp_title[0].find_element_by_tag_name('span')
+        while title.text == "":
+            pass
+        detail_nama_toko = title.text
+        print("Nama Toko : ")
+        print(detail_nama_toko)
+        detail["nama"] = detail_nama_toko
 
-    title = temp_title[0].find_element_by_tag_name('span')
-    while title.text == "":
-        pass
-    detail_nama_toko = title.text
-    print("detail_nama_toko : ")
-    print(detail_nama_toko)
-    detail["nama"] = detail_nama_toko
-
-    btn_detail = driver.find_elements_by_class_name('ugiz4pqJLAG__button')
-    # Val 1 alamat, 2 notelp
-    counting_val = 0 
-    for btn in btn_detail:
-        val = btn.get_attribute("aria-label")
-        if val is not None:
-            num = not "." in val and not "," in val 
-        else:
-            num = False
-        if counting_val == 0:
-            detail["alamat"] = val
-            print("alamat:")
-            print(val)
-        elif num:
-            detail["notelp"] = val
-            print("notelp:")
-            print(val)
+        btn_detail = driver.find_elements_by_class_name('ugiz4pqJLAG__button')
+        # Val 1 alamat, 2 notelp
+        counting_val = 0 
+        for btn in btn_detail:
+            val = btn.get_attribute("aria-label")
+            if val is not None:
+                num = not "." in val and not "," in val 
+            else:
+                num = False
+            if counting_val == 0:
+                detail["alamat"] = val
+            elif num:
+                detail["notelp"] = val
+            
+        all_data.append(detail)
         counting_val += 1
-
-    all_data.append(detail)
+    except :
+        pass
+    
     class_close = 'sbcb_a'
     driver.find_element_by_class_name(class_close).click()
 
+wb = Workbook()
+sheet = wb.active
+counter_data = 1
+sheet.append(("No","Nama Toko","Notelp","Alamat"))
+for item in all_data:
+    nama = item["nama"]
 
+    alamat = item["alamat"]
+    alamat = alamat.split(": ")
+    alamat = alamat[1]
+    try:
+        notelp = item["notelp"]
+        notelp = notelp.split(": ")
+        notelp = notelp[1]
+    except:
+        notelp = ""
+    
+    sheet.append((counter_data,nama,notelp,alamat))
+    counter_data += 1
+current_time = time.time()
+timez = str(current_time).split('.')
+task_id =  timez[0]
+path = "%s.xlsx" % (task_id)
+print(path)
+wb.save("data/"+path)
 # ActionChains(driver).key_up(Keys.ENTER).perform()
+driver.close()
