1733496c75c7c333e965dfd2ddd058c1a4dd9362
Rizky Novriansyah <rizkynovriansyah82.rn@gmail.com>
2020-10-29 12:28:04 +0700

    get notelp & lokasi

--- scrap.py
+++ scrap.py
@@ -9,51 +9,113 @@ driver.get('https://www.google.com/maps')
 
 # kota = input('kota : ')
 # key = input('key : ')
-kota = "Malang"
+kota = "Jakarta"
 key = "Toko Sepatu"
 
-final_msg = key+", "+kota
-ActionChains(driver).send_keys(final_msg).perform()
+find_key = key+", "+kota
+ActionChains(driver).send_keys(find_key).perform()
 class_find = 'searchbox-searchbutton'
 driver.find_element_by_class_name(class_find).click()
 
-# 
+#
 # time.sleep(4)
 class_item = 'section-result'
 items = []
-while len(items) == 0 :
+while len(items) == 0:
     items = driver.find_elements_by_class_name(class_item)
 
 print(len(items))
 count = 0
 # print(items)
+nama_tokos = []
 for item in items:
-    count += 1
-    print("== Detail Item ==")
-    item.click()
-    print("Title : ")
+    temp_title = []
+    while len(temp_title) == 0:
+        temp_title = item.find_elements_by_class_name('section-result-title')
+    title = temp_title[0].find_element_by_tag_name('span')
+    nama_toko = title.text
+    nama_tokos.append(nama_toko)
+
+class_close = 'gsst_a'
+driver.find_element_by_class_name(class_close).click()
+
+all_data = []
+for nama_toko in nama_tokos:
+    final_msg = nama_toko+", "+kota
+    if final_msg == find_key:
+        continue
     
-    # time.sleep(4)
-    val = []
-    while len(val) == 0 :
-        val = driver.find_elements_by_class_name('section-hero-header-title-title')
-    title = val[0].find_element_by_tag_name('span')
-    print(title.text)
+    print("final_msg : ")
+    print(final_msg)
+    ActionChains(driver).send_keys(final_msg).perform()
+    class_find = 'searchbox-searchbutton'
+    driver.find_element_by_class_name(class_find).click()
 
-    print("Back To list")
-    class_back = 'section-back-to-list-button'
-    driver.find_element_by_class_name(class_back).click()
-    time.sleep(4)
+    temp_title = []
+    error = 0
+    while len(temp_title) == 0:
+        temp_title = driver.find_elements_by_class_name('section-hero-header-title-title')
+        try:
+            temp_titles = []
+            temp_titles = driver.find_elements_by_class_name('section-result-title')
+            if len(temp_titles) > 0 :
+                is_not_exist = True
+                for t in temp_titles:
+                    title = t.find_element_by_tag_name('span')
+                    title_temp = title.text 
+                    print(title_temp)
+                    if title_temp == nama_toko:
+                        t.click() 
+                        is_not_exist = False
+                        break   
+                if is_not_exist:
+                    temp_titles[0].click() 
+            
+        except:
+            error += 1
+            if error > 5:
+                print(x)
+        
+    detail = {}
 
-    class_item = 'section-result'
-    items = driver.find_elements_by_class_name(class_item)
-    # time.sleep(4)
+    title = temp_title[0].find_element_by_tag_name('span')
+    while title.text == "":
+        pass
+    detail_nama_toko = title.text
+    print("detail_nama_toko : ")
+    print(detail_nama_toko)
+    detail["nama"] = detail_nama_toko
+    # lokasi_toko = driver.find_elements_by_class_name('ugiz4pqJLAG__primary-text')
+    # while lokasi_toko.text == "":
+    #     pass
+    # print("lokasi_toko.text : ")
+    # print(lokasi_toko.text)
 
-    for x in range(count):
-        items.pop(0)
-    
-    print(len(items))
-
+    btn_detail = driver.find_elements_by_class_name('ugiz4pqJLAG__button')
+    print("lokasi_toko")
+    print(len(btn_detail))
+    # Val 1 alamat, 2 notelp
+    counting_val = 0 
+    for btn in btn_detail:
+        val = btn.get_attribute("aria-label")
+        # btn.find_element_by_tag_name('span')
+        if counting_val == 0:
+            detail["alamat"] = val
+            print("alamat:")
+            print(val)
+        elif counting_val == 1:
+            detail["notelp"] = val
+            print("notelp:")
+            print(val)
+        counting_val += 1
+        
+    # while lokasi_toko.text == "":
+    #     pass
+    # print("lokasi_toko.text : ")
+    # print(lokasi_toko.text)
+    all_data.append(detail)
+    class_close = 'sbcb_a'
+    driver.find_element_by_class_name(class_close).click()
 
 
 # ActionChains(driver).key_up(Keys.ENTER).perform()
