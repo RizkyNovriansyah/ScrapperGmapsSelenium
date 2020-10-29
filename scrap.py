637625f5ec676f7764f2f873c77da089466874f9
Rizky Novriansyah <rizkynovriansyah82.rn@gmail.com>
2020-10-29 10:29:16 +0700

    get all result

--- scrap.py
+++ scrap.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -6,11 +7,24 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.google.com/maps')
 
-kota = input('kota : ')
-key = input('key : ')
+# kota = input('kota : ')
+# key = input('key : ')
+kota = "Malang"
+key = "Toko Sepatu"
 
 final_msg = key+", "+kota
 ActionChains(driver).send_keys(final_msg).perform()
 
-driver.find_element_by_class_name('searchbox-searchbutton').click()
+class_find = 'searchbox-searchbutton'
+driver.find_element_by_class_name(class_find).click()
+
+# 
+time.sleep(4)
+class_item = 'section-result'
+items = driver.find_elements_by_class_name(class_item)
+time.sleep(2)
+print(len(items))
+# print(items)
+
+
 # ActionChains(driver).key_up(Keys.ENTER).perform()
